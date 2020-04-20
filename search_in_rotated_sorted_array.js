b719e33dcce38c3ccb9a0fbe3566c7a776574a26
Gaku Sasaki <gaku.sasaki@gmail.com>
2020-04-20 15:55:05 -0700

    day 20

--- search_in_rotated_sorted_array.js
+++ search_in_rotated_sorted_array.js
@@ -10,21 +10,50 @@ Your algorithm's runtime complexity must be in the order of O(log n).
 
 
 /* Approach: 
-    Slowest possible search, REDO THIS LOL
+    Assume that either rotated half will still be sorted
 */
-var search = function(nums, target) {
-    for (var idx in nums){
-        if (nums[idx] === target) return idx;
+var search = function (nums, target) {
+    // Left edge of search scope
+    let left = 0;
+    // Right edge of search scope
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        // find middle index
+        let mid = Math.floor((left + right) / 2);
+
+        // Return if target was found
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            // Left side of middle is sorted. Is target in that range?
+            if (nums[left] <= target && target <= nums[mid]) {
+                // Yes, so shrink the search to the left half and repeat loop
+                right = mid - 1;
+            } 
+            else {
+                // Target is in the right, shrink search to the right half and repeat loop
+                left = mid + 1;
+            }
+        }
+        // Same logic, except mirrored
+        else {
+            if (nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
     }
+    // While loop exited MEANS search scope became illegal MEANS target was never found
     return -1;
 };
 
-/*
-    (Solution or Best Answer from Discussion Board)
-*/
-// {{ SOLUTION CODE IF CODED OUT}}
 
-let nums = [4,5,6,7,0,1,2];
+let nums = [4, 5, 6, 7, 0, 1, 2];
 let target = 0;
 console.log(search(nums, target));
 //Expected: 4
